0c1f265470b0c8df93ff5cd0feb9c0f57f5c3d84
Nolan Burfield <nburf@nevada.unr.edu>
2015-08-31 21:08:02 -0700

    another error fix

--- config/breadcrumbs/upload_data.rb
+++ config/breadcrumbs/upload_data.rb
@@ -3,6 +3,8 @@ crumb :upload_datum do |upload_datum|
   course = upload_datum.source.assignment.course
   if upload_datum.test_case.present? and current_user.has_local_role? :grader, course then
     parent :test_case, upload_datum.test_case
+  elsif upload_datum.test_case.present? and current_user.has_role? :admin
+    parent :test_case, upload_datum.test_case
   else
     parent :submission, upload_datum.submission
   end
